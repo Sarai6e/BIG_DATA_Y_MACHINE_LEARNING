c24f3c3e6625df5a271224860bdfb7099042e601
Sarai6e <1472173@senati.pe>
2024-11-15 12:58:34 -0500

    Matriz que de 10 datos encuentra el numero 0 usando Numpy ..

--- 15-11-24/Programacion con deteccion de objetos/Matriz de 10 datos donde se encuentra el numero 0.py
+++ 15-11-24/Programacion con deteccion de objetos/Matriz de 10 datos donde se encuentra el numero 0.py
@@ -1,5 +1,9 @@
 import numpy as np
 
-matriz = np.array([int(input(f"Ingrese el número {i+1}: ")) for i in range(10)])
+# Ingresar 9 números y agregar 0 al final
+matriz = np.array([int(input(f"Ingrese el número {i+1}: ")) for i in range(9)] + [0])
+
+# Buscar la posición del número 0
 for i, num in enumerate(matriz):
-    if num == 0: print(f"El número 0 está en el orden {i+1}, posición {i}")
+    if num == 0:
+        print(f"El número 0 está en el orden {i+1}")
