74c11a390d3a4f31e757d5a08b4449722ae5e7f3
Sarai6e <1472173@senati.pe>
2024-11-15 10:37:53 -0500

    orden de menor a mayor.

--- 15-11-24/Programacion clásica/ordenar numeros de menor a mayor.py
+++ 15-11-24/Programacion clásica/ordenar numeros de menor a mayor.py
@@ -1,6 +1,2 @@
-# Ingresar 4 números y ordenarlos
-n = sorted([int(input("Ingrese un número: ")) for _ in range(4)])
-
-# Mostrar la lista ordenada
-print("Lista ordenada de menor a mayor:", n)
+print(sorted([int(input("Número: ")) for _ in range(4)]))
 
